bc572d75576b18c1ff799d3f0dbc466ade244e49
DenDmik <dendmik@gmail.com>
2025-03-13 15:51:07 +0200

    проверка работы приложения в tg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from "react";
+
 
 
 export default function Home() {
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    // @ts-expect-error проверка
+    if (typeof window !== 'undefined' && window.Telegram?.WebApp) {
+      // @ts-expect-error проверка
+      const tg = window.Telegram.WebApp;
+      tg.ready();
+      setIsReady(true);
+    }
+  }, []);
+
+  if (!isReady) return null; 
+
+
+// @ts-expect-error проверка
+const handleClose=()=>{window.Telegram.WebApp.close()}
   return (
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center ">
         <div className="text-red-700 text-4xl text-center">HELLO</div>
+        <button 
+        className="text-2xl text-amber-300"
+        onClick={handleClose} 
+
+        >Close</button>
 
       </main>
   );
